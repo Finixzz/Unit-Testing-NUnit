d2035b6d73494c0596023f2d68a21369b56ac359
Ahmet Durmic <ahmetdurmic97@outlook.com>
2021-04-20 10:54:39 +0200

    Implemented SetUp method and parameterized test example

--- SampleProject/SampleProject.UnitTests/Fundamentals.Tests/MathTests.cs
+++ SampleProject/SampleProject.UnitTests/Fundamentals.Tests/MathTests.cs
@@ -11,16 +11,65 @@ namespace SampleProject.UnitTests.Fundamentals.Tests
     [TestFixture]
     public class MathTests
     {
+        private Math _math;
+
+        //SetUp
+        //TearDown
+
+        [SetUp]
+        public void SetUp()
+        {
+            //Arrange
+            _math = new Math();
+        }
+
 
         [Test]
         public void Add_WhenCalled_ReturnsSumOfArguments()
         {
-            //Arrange
-            Math math = new Math();
             //Act
-            var result = math.Add(1, 3);
+            var result = _math.Add(1, 2);
             //Assert
-            Assert.AreEqual(result, 4);
+            Assert.AreEqual(result, 3);
+        }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnsTheFirstArgument()
+        {
+
+            var result = _math.Max(2, 1);
+
+            Assert.AreEqual(result, 2);
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnsTheSecondArgument()
+        {
+
+            var result = _math.Max(1, 2);
+
+            Assert.AreEqual(result, 2);
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnsTheSameArguments()
+        {
+
+            var result = _math.Max(1, 1);
+
+            Assert.AreEqual(result, 1);
+        }
+
+        //Parameterized tests
+        [Test]
+        [TestCase(2,1,2)]
+        [TestCase(1,2,2)]
+        [TestCase(1,1,1)]
+        public void Max_WhenCalled_ReturnGreatherArgument(int a,int b,int expectedResult)
+        {
+            var result = _math.Max(a, b);
+
+            Assert.AreEqual(result, expectedResult);
         }
     }
 }
