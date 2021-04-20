0858dea82e828f3ed0f3dc463589347bd9a9d773
Ahmet Durmic <ahmetdurmic97@outlook.com>
2021-04-20 10:05:16 +0200

    Setted up environment, created first unit test

---
+++ SampleProject/SampleProject.UnitTests/Fundamentals.Tests/MathTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Math=SampleProject.Fundamentals.Math;
+
+namespace SampleProject.UnitTests.Fundamentals.Tests
+{
+    [TestFixture]
+    public class MathTests
+    {
+
+        [Test]
+        public void Add_WhenCalled_ReturnsSumOfArguments()
+        {
+            //Arrange
+            Math math = new Math();
+            //Act
+            var result = math.Add(1, 3);
+            //Assert
+            Assert.AreEqual(result, 4);
+        }
+    }
+}
