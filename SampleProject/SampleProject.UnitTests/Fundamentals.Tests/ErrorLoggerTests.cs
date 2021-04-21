1e0d2d6c3dfc5e17ba6169ac6f6806e2325579f6
Ahmet Durmic <ahmetdurmic97@outlook.com>
2021-04-21 11:47:39 +0200

    Finished sample of testing void methods

---
+++ SampleProject/SampleProject.UnitTests/Fundamentals.Tests/ErrorLoggerTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using SampleProject.Fundamentals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProject.UnitTests.Fundamentals.Tests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_ShouldSetLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("error");
+
+            Assert.That(logger.LastError, Is.EqualTo("error"));
+        }
+    }
+}
---
+++ SampleProject/SampleProject/Fundamentals/ErrorLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProject.Fundamentals
+{
+    public class ErrorLogger
+    {
+        public string LastError { get; set; }
+
+        public event EventHandler<Guid> ErrorLogged;
+
+        public void Log(string error)
+        {
+            if (String.IsNullOrWhiteSpace(error))
+                throw new ArgumentNullException();
+
+            LastError = error;
+
+            ErrorLogged?.Invoke(this, Guid.NewGuid());
+        }
+    }
+}
